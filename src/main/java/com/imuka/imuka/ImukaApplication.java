57289dcbbdfcc8f4d320be9254a03302d5db1a11
tendomart <tmkmart@gmail.com>
2019-07-22 14:24:28 +0300

    Created Imuka Access Repo

---
+++ src/main/java/com/imuka/imuka/ImukaApplication.java
@@ -0,0 +1,13 @@
+package com.imuka.imuka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImukaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imuka/imuka/ImukaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imuka.imuka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImukaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
